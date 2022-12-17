c40b97d923a6b21ba6c1870a952cc14d5998b564
Kekray <kekray777@gmail.com>
2022-12-17 16:19:34 +0400

    added github source link

--- src/components/UI/head/MyHead.tsx
+++ src/components/UI/head/MyHead.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import cl from './MyHead.module.css';
 import { BiNotepad } from 'react-icons/bi';
 import { SiTypescript, SiReact } from 'react-icons/si';
-
+import MyButton from '../button/MyButton';
+import { SiGithub } from 'react-icons/si';
 const MyHead = () => {
   return (
     <div className={cl.head}>
@@ -13,6 +14,13 @@ const MyHead = () => {
           <SiReact id={cl['react']} />
           <SiTypescript id={cl['typescript']} />
         </span>
+        <a
+          href='https://github.com/FortuneHere/TodoApp-dnd-tsx'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <SiGithub />
+        </a>
       </p>
     </div>
   );
